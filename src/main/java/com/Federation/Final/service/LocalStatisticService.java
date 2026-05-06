c36c7289ab7925d140ee4cb20a3bef724b32acf5
mioty <hei.mioty.2@gmail.com>
2026-05-06 15:30:53 +0300

    Feat : Add statistic by collectivity implementation

---
+++ src/main/java/com/Federation/Final/service/LocalStatisticService.java
@@ -0,0 +1,34 @@
+package com.Federation.Final.service;
+
+import com.Federation.Final.entity.dto.CollectivityLocalStatistics;
+import com.Federation.Final.repository.LocalStatisticRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+public class LocalStatisticService {
+
+
+    private LocalStatisticRepository localStatisticRepository;
+
+    public LocalStatisticService(LocalStatisticRepository localStatisticRepository){
+        this.localStatisticRepository = localStatisticRepository;
+    }
+
+    public List<CollectivityLocalStatistics> getLocalStatistics(String collectivityId,
+                                                                LocalDate from,
+                                                                LocalDate to) {
+        try {
+            // Appel direct au repository
+            return localStatisticRepository.getLocalStatistics(collectivityId, from, to);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Erreur lors de la récupération des statistiques: " + e.getMessage());
+        }
+    }
+}
